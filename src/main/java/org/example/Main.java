8900df301d607d830b89c7e14c2a744d242aa57c
ovk-lviv <ovk.16.07.2019@gmail.com>
2024-07-03 00:01:14 +0300

    hometask10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,8 @@ package org.example;
 import java.util.List;
 
 public class Main {
-    public static BankApplication ba = new BankApplication();
+    private static BankApplication ba = new BankApplication();
+
 
     static public void main(String[] args) {
         /*
@@ -14,39 +15,31 @@ public class Main {
             id = accountId001, amount 50, currency USD
             id = accountId001, amount 200, currency USD
          */
-        // Some changes
         try {
-
-            processWrapper("001", 200, "USD");
-        } catch (WrongAccountException wae) {
-            System.out.println("Wrong account!");
-        } catch (WrongOperationException woe) {
-            System.out.println("Insufficient funds!");;
-        } catch (WrongCurrencyException wce) {
-            System.out.println("Wrong currency");
-        }
-        catch (Exception e){
-            System.out.println("An error occurred. Please try again");
-        }finally {
+            processWrapper("accountId001", 100, "USD");
+        } catch (WrongAccountException a) {
+            System.out.println(a.getMessage());
+        } catch (WrongCurrencyException c) {
+            System.out.println(c.getMessage());
+        } catch (WrongOperationException o) {
+            System.out.println(o.getMessage());
+        } catch (Exception e) {
+            System.out.println("Something went wrong!");
+        } finally {
             System.out.println("Thank you for using our service!");
         }
-
     }
 
     public static void processWrapper(String s, int i, String s1) throws Exception {
-//який буде викликати метод process об’єкту BankApplication.
         ba.process(s, i, s1);
         if (s == null) {
             throw new WrongAccountException();
+        }
+        if (!s1.equals("USD") && !s1.equals("HRV") && !s1.equals("EUR")) {
+            throw new WrongCurrencyException();
         }
         if (i < 0) {
             throw new WrongOperationException();
         }
 
-        if (!s1.equals("EUR") && !s1.equals("USD") && !s1.equals("HRV")) {
-            throw new WrongCurrencyException();
-        }
-
-
-    }
-}
+    }}
